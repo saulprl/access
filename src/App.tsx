9210527b82e45cf8c946f81bde85e1bb1dd44901
saulprl <phasecross@hotmail.com>
2023-07-13 23:00:07 -0700

    Add fonts, prettier, tailwind theme, sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { MdEmail } from "react-icons/md";
+
+import accessLogo from "./assets/csipro-access.png";
+import github from "./assets/auth-providers/github-mark.png";
+import google from "./assets/auth-providers/google-g.png";
+import { IconContext } from "react-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="absolute h-screen w-screen max-w-md overflow-hidden bg-primary-700 pb-8 pt-20 text-slate-200 md:left-[50%] md:top-[50%] md:h-auto md:translate-x-[-50%] md:translate-y-[-50%] md:rounded-lg">
+      <img src={accessLogo} className="w-full" />
+      <div className="mt-40 flex flex-col gap-4 md:mt-20">
+        <SignInButton provider="GitHub" />
+        <SignInButton provider="Google" />
+        <SignInButton provider="email" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const providerLogos = {
+  GitHub: (
+    <span className="text-github">
+      <FaGithub />
+    </span>
+  ),
+  Google: <FcGoogle />,
+  email: (
+    <span className="text-zinc-400">
+      <MdEmail />
+    </span>
+  ),
+};
+
+const SignInButton = (props: { provider: "GitHub" | "Google" | "email" }) => {
+  const { provider } = props;
+
+  const [buttonText, setButtonText] = useState(`Sign in with ${provider}`);
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  const handleClick = () => {
+    if (provider === "email") {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      setButtonText("Nope - wrong option");
+      timer = setTimeout(() => {
+        setButtonText(`Sign in with ${provider}`);
+      }, 5000);
+    }
+  };
+
+  return (
+    <IconContext.Provider value={{ className: "text-3xl" }}>
+      <button
+        className="cursor relative mx-8 h-14 rounded-full bg-slate-300 py-4 font-auth font-medium text-primary-700 transition-colors hover:brightness-100 active:bg-primary-50"
+        onClick={handleClick}
+      >
+        <span className="absolute -top-1 left-0 flex h-full w-full flex-row items-center justify-center rounded-full bg-white pl-8 transition-transform active:translate-y-1 active:bg-primary-50">
+          <span className="absolute left-6">{providerLogos[provider]}</span>
+          <span className="">
+            <span className="mx-auto">{buttonText}</span>
+          </span>
+        </span>
+      </button>
+    </IconContext.Provider>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#d4c7f3",
+          100: "#c6b5ef",
+          200: "#b8a2eb",
+          300: "#aa8fe6",
+          400: "#9c7de2",
+          500: "#8d6ade",
+          600: "#7f58da",
+          700: "#7145d6",
+          800: "#663ec1",
+          900: "#5a37ab",
+        },
+        secondary: "#e91e63",
+        tertiary: "#0080ff",
+        error: "#ff6f00",
+        github: "#171515",
+      },
+      fontFamily: {
+        auth: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
